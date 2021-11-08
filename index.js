eb9ff1472239b87c679d068d2c2f20d2343af848
aaahana <e10991aaahana@dpsrkp.net>
2021-11-08 14:04:34 +0530

    initial

---
+++ index.js
@@ -0,0 +1,12 @@
+// const person1 = require('./person');
+// console.log(person1.name)
+
+
+//-------------------------------------
+
+
+const Person = require("./person");
+
+const person2 = new Person('John Doe', 30)
+
+person2.greeting()
